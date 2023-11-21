de5d7239825b8d7d3c876a80cc87a37be7240fb8
sh6ta-thecat <145577471+sh6ta-thecat@users.noreply.github.com>
2023-11-21 12:03:49 -0500

    Update results_input.js

--- includes/js/search/results_input.js
+++ includes/js/search/results_input.js
@@ -1,1 +1,6 @@
+const urlParams = new URLSearchParams(window.location.search);
+const searchTerm = urlParams.get('q');
+const searchType = urlParams.get('type');
 
+document.getElementById('search-header').innerText = `Resultados de "${searchTerm}"`
+document.getElementById('search-input').value = searchTerm;
